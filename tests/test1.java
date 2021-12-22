23e2a0d5c817a9cb0acce5459da4c23885328c3c
williamsirotkin <williamsirotkin@gmail.com>
2021-12-22 16:33:24 -0500

    autofill works with files

--- tests/test1.java
+++ tests/test1.java
@@ -1,17 +1,29 @@
 import java.lang.Exception;
+import binarysearch.FileToStringArray;
+import java.io.File;
 
 public class test1 {
 
     public static void main(String[] args) {	
 	String input;
 	AutoFill af;
+	File file;
+	FileToStringArray ftsa;
+	String[] strArray;
 
 	try {
-	    input = args[0];
-	    af = new AutoFill();
-	    System.out.println(af.autoFill(args[0]));
+	    file = new File(args[0]);
+	    ftsa = new FileToStringArray();
+	    strArray = ftsa.fileToStringArray(file);
+
+	    System.out.println(strArray.length);
+
+	    for (int i = 0; i < strArray.length; i++) {
+		af = new AutoFill();
+		System.out.println(af.autoFill(strArray[i]));
+	    } // for
 	} catch (Exception e) {
-	    System.out.println("ERROR: Need A Command Line Argument!");
+	    System.out.println("ERROR: EXCEPTION IN TEST1 CLASS!");
 	} // try/catch
 
 	
